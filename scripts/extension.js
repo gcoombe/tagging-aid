c0b8b325ebbf3acfd59d4b60c1b52cb4bae3fe58
Mike Fotinatos <michael.fotinatos@gmail.com>
2019-09-18 18:26:51 -0400

    Basic tagging aid UI works, but won't access page DOM

---
+++ scripts/extension.js
@@ -0,0 +1,60 @@
+//Invoke the Mouseover event immediately
+var _id_ = "";
+var _classNames_ = [];
+
+function startMouseover(){
+  document.getElementById('status').textContent = "Ready to inspect!";
+  
+  window.onmouseover=(function(e) {
+    _id_ = e.target.id;
+    _classNames_ = e.target.className.split(" ");
+
+    var appendedHTML = "";
+
+    $('#id-result').val("#" + _id_);
+    $('#class-result-0').val("." + _classNames_[0]);
+    $("#template-table").empty();
+    
+    for (i=1; i < _classNames_.length; i++) {
+      appendedHTML = appendedHTML +
+      '<tr>' +
+        '<td width="90%" class="input-row"><input class="form-control class-result" type="text" id="class-result-' + i + '" value=".' + _classNames_[i] + '" readonly></td>' +
+        '<td width="2%" class="input-row">&nbsp;</td>' +
+        '<td width="8%" class="input-row">' +
+          '<div onclick=\'copyToClipboard("class-result-' + i + '", ".");\'>' +
+            '<a href="#"><img src="./copy_icon.ico" width="20"></a>' +
+          '</div>' +
+        '</td>' +
+        '</tr>';
+    }
+
+    if(_classNames_.length > 1) {
+      $("#template-table").html(appendedHTML);
+    }  
+  });
+};
+
+window.onclick = function (e) {
+  if(window.onmouseover != null) {
+    document.getElementById('status').textContent = "Element Locked.  Click anywhere to reset.";
+    window.onmouseover = null;
+  } else {
+    startMouseover();
+  }
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+  (startMouseover());
+});
+
+function copyToClipboard(inputId, inputType) {
+  /* Get the text field */
+  var copyText = document.getElementById(inputId);
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+}
