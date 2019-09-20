28e0bbc1766374b4f22a3e4b28d5c11cf198ee3a
connellyguy <joshua@joshc.net>
2019-09-20 11:02:11 -0400

    removed redundancies

--- js/background.js
+++ js/background.js
@@ -1,11 +1,11 @@
 chrome.browserAction.onClicked.addListener(function (tab) {
   // for the current tab, inject the "inject.js" file & execute it
 
-    chrome.tabs.executeScript(tab.ib, {
+    chrome.tabs.executeScript(tab.id, {
     file: './src/scripts/jquery.min.js'
     });
     
-    chrome.tabs.executeScript(tab.ib, {
+    chrome.tabs.executeScript(tab.id, {
 		file: './src/scripts/insertUI.js'
     });
 
